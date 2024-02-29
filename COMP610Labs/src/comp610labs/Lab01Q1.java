fd5a1027f74d96c941b737ddac23b4a8b6a4c50f
Mark <test@email.com>
2024-02-29 21:39:16 +1300

    Repasted lab java files to Netbeans14 JDK 11

---
+++ COMP610Labs/src/comp610labs/Lab01Q1.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package comp610labs;
+
+/**
+ *
+ * @author mark
+ */
+public class Lab01Q1 
+{
+    /**
+     * @param args the command line arguments
+     */	
+    public static void main(String[] args) 
+    {
+       diamond(6, 6);
+    }
+    
+    // prints the *
+    public static void repeatlyPrint(int amount, String pattern)
+    {
+        if(amount > 0)
+        {
+            System.out.print(pattern);
+            
+            repeatlyPrint(amount - 1, pattern);
+        }
+    }
+    
+    // prints the shape
+    public static void diamond(int totalHeight, int currentRow)
+    {
+        repeatlyPrint(totalHeight, " ");
+        repeatlyPrint(totalHeight - currentRow, "*" + " ");
+        
+        if(totalHeight > 0)
+        {
+            diamond(totalHeight - 1, currentRow + 1);
+        }
+        
+        repeatlyPrint(totalHeight - currentRow, " ");
+        repeatlyPrint(totalHeight, "*" + " ");
+        
+        System.out.println("");
+    }
+    
+}
