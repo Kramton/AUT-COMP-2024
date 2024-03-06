e647aadb62f7d2e75143436d8a5e8aedefd8fa81
Mark <test@email.com>
2024-03-06 20:48:17 +1300

    Update test.java

--- COMP610Labs/src/test.java
+++ COMP610Labs/src/test.java
@@ -9,35 +9,11 @@
  */
 public class test 
 {
-   public static void main(String[] args) {
-        int[] numbers = {10, 15, 25, 43};
-
-        // Call the method to insert a number into the array
-        InsertToArray(7, numbers, numbers.length);
-
-        // Print the array after insertion
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.print(numbers[i] + " ");
-        }
+    public static void main(String[] args)
+    {
+        Thread t2 = new Thread();
+        
+        System.out.println(t2.getId());
     }
-
-    public static void InsertToArray(int number, int[] list, int size) {
-        int index = size; // Initialize index to size (end of the array)
-
-        // Find the right position for inserting a number and store the position in index
-        for (int i = 0; i < size; i++) {
-            if (list[i] > number) {
-                index = i;
-                break;
-            }
-        }
-
-        // Shift numbers to make space to insert the new number
-        for (int i = size - 1; i > index; i--) {
-            list[i] = list[i - 1];
-        }
-
-        // Insert the new number
-        list[index] = number;
-    }
+        
 }
