9c887c2aea01c84c5ca1667a358114874cdb7f6f
Mark <test@email.com>
2024-03-12 19:10:22 +1300

    completed task2_1

--- COMP610Labs/src/test.java
+++ COMP610Labs/src/test.java
@@ -9,10 +9,15 @@
  */
 public class test 
 {
-    public static void main(String[] args)
-    {
-        Thread t2 = new Thread();
-        
-        System.out.println(t2.getId());
+    public static void main(String[] args) {
+        String originalStr = "Hello";
+        String reversedStr = "";
+        System.out.println("Original string: " + originalStr);
+
+        for (int i = 0; i < originalStr.length(); i++) {
+            reversedStr = originalStr.charAt(i) + reversedStr;
+        }
+
+        System.out.println("Reversed string: " + reversedStr);
     }
 }
